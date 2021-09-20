6efa56a4b4c90385005e0d496664aa684677d0e1
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-09-20 13:56:30 +0000

    [Add] 他ユーザーのpost一覧

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,15 +9,15 @@ class Post < ApplicationRecord
 
   def save_tag(sent_tags)
     current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
-    old_tags = current_tags - sent_tags
-    new_tags = sent_tags - current_tags
+    old_tags = current_tags - sent_tags # 現在取得した@postに存在するタグから、送信されてきたタグを除いたタグをold_tagsとする。
+    new_tags = sent_tags - current_tags # 送信されてきたタグ(sent_tag)から、現在存在するタグを除いたタグをnew_tagsとする。
 
     old_tags.each do |old|
       self.tags.delete Tag.find_by(tag_name: old)
     end
 
     new_tags.each do |new|
-      new_post_tag = Tag.find_or_create_by(tag_name: new)
+      new_post_tag = Tag.find_or_create_by(tag_name: new) # tag_nameが存在していなければ、新たにレコード追加、既に存在する場合は既存のレコードを取得
       self.tags << new_post_tag
     end
   end
