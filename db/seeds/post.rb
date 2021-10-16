a9b4d68483189e5bcc5d1c2074f929bb61464ff8
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-10-16 12:37:40 +0000

    [Add] seed.rbの追加

--- db/seeds/post.rb
+++ db/seeds/post.rb
@@ -7,3 +7,31 @@
       user_id: "#{n + 1}"
     )
   end
+
+
+  10.times do |n|
+    Post.create!(
+      body: Faker::Lorem.sentence,
+      image: open("#{Rails.root}/db/fixtures/mateus-campos-felipe-bWp6UhjOcBg-unsplash.jpg"),
+      jeans_id: "#{n + 1}",
+      user_id: "#{n + 1}"
+    )
+  end
+
+  10.times do |n|
+    Post.create!(
+      body: Faker::Lorem.sentence,
+      image: open("#{Rails.root}/db/fixtures/mohamad-khosravi-d54wbtjedog-unsplash.jpg"),
+      jeans_id: "#{n + 1}",
+      user_id: "#{n + 1}"
+    )
+  end
+
+  10.times do |n|
+    Post.create!(
+      body: Faker::Lorem.sentence,
+      image: open("#{Rails.root}/db/fixtures/jason-leung-EtOMMg1nSR8-unsplash.jpg"),
+      jeans_id: "#{n + 1}",
+      user_id: "#{n + 1}"
+    )
+  end
