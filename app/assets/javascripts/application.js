50d2d2072c36f4ac8b9e5311c9141b6b98403a5a
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-10-17 12:05:42 +0000

    [Add] お気に入り非同期化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -244,4 +244,45 @@
         // instead of a settings object
       ]
   });
+  $('.slick_logged_in').slick({
+      infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,  // ←この階層
+        autoplay: true,
+        autoplaySpeed: 0,　//隣あう画像のスライドするまでの間隔時間
+        speed: 10000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        adaptiveHeight: true,
+        cssEase: 'linear', //開始から終了まで一定に変化する
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
 });
