e3502e5101c110988bb625296a20c9d3fb6be0c8
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-10-18 11:55:38 +0000

    [Update] コメント非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build  # 投稿全体へのコメント投稿用の変数
+    @comment_reply = @post.comments.build  # コメントに対する返信用の変数
+    @comments = Comment.includes(:user).where(post_id: @post.id).order(created_at: :desc)
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to request.referer || root_path
   end
 
   def destroy
