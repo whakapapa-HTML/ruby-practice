735f40f6e766318b921994e42de03280d4d1d289
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-10-20 08:04:42 +0000

    [Fix] トップ画面の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,10 @@ class PostsController < ApplicationController
       @posts = Post.all
     else
       @posts = Post.all.shuffle
+      # 一週間分のレコードを取得
+      from  = Time.current.at_beginning_of_day
+      to    = (from - 6.day).at_end_of_day
+      @winners = AwardMap.where(created_at: to...from)  # ...でfrom 以上 to 未満
     end
   end
 
--- app/models/award_map.rb
+++ app/models/award_map.rb
@@ -1,6 +1,8 @@
 class AwardMap < ApplicationRecord
 
   belongs_to :jeans
-  belongs_to :award
+  belongs_to :award, optional: true
+
+  enum award_id: { "Best Faded": 1, "Best Fashionable": 2, "Best Whisker": 3, "Best Shevron": 4, "Best 501": 5 }
 
 end
