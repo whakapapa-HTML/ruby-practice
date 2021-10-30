817addfb6c7106ac45e5c7c3628601c1017e7dea
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-10-30 14:27:14 +0000

    Use Ruby 2.6.6

--- db/migrate/20211030082523_specify_column_name.rb
+++
@@ -1,9 +0,0 @@
-class SpecifyColumnName < ActiveRecord::Migration[5.2]
-  def up
-    change_column :nominees, :name, :integer, using: "name::integer"
-  end
-
-  def down
-    change_column :nominees, :name, :integer
-  end
-end
