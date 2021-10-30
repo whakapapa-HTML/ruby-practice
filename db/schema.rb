2fa398974cb49e6d2583f96d8ace9e82f473989f
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-10-30 08:18:38 +0000

    [Add] nameカラムの型を明記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_134023) do
+ActiveRecord::Schema.define(version: 2021_10_30_081558) do
 
   create_table "award_maps", force: :cascade do |t|
     t.integer "award_id"
